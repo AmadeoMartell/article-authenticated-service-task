bf30fc30a0f62a9d62601b9ee5f227e04d43e6f7
Vadim Markov <vadim_markov@epam.com>
2024-04-29 02:03:43 +0200

    Initial

---
+++ src/main/java/edu/epam/fop/spring/boot/dbsecurity/SpringBootDbSecurityApplication.java
@@ -0,0 +1,13 @@
+package edu.epam.fop.spring.boot.dbsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDbSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDbSecurityApplication.class, args);
+    }
+
+}
