4bfee946803dbe60702b44e5c68f66697310c84b
AmadeoMartell <medeu207@gmail.com>
2025-05-09 16:04:26 +0500

    Implementing UserDetailService, configuring SecurityConfig

---
+++ src/main/java/edu/epam/fop/spring/boot/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package edu.epam.fop.spring.boot.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService customUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,   "/article/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,    "/article/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/article/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form.disable())
+                .logout(logout -> logout.disable())
+                .userDetailsService(customUserDetailsService);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
